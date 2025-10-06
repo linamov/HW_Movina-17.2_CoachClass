17bf801ba9212df11f041c59702028c1ddf2affa
linamov <linusikvann@gmail.com>
2025-10-06 22:44:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ class Coach {
   }
 }
 
-const coach1 = new Coach('John Wodden', 'Fitness', 4.7);
-const coach2 = new Coach('Alice Chan', 'Yoga', 4.9);
+const coach1 = new Coach('John Doe', 'Fitness', 4.7);
+const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
 
 coach1.displayInfo(); 
 coach2.displayInfo(); 
